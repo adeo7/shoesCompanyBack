09ceac3d5b48abe89759f5710e33c5bd97a7803b
adeo7 <bdmosquera7@misena.edu.co>
2023-12-14 23:50:59 -0500

    Listo para produccion

--- shoes/settings/production.py
+++ shoes/settings/production.py
@@ -1,19 +1,15 @@
 from .base import *
-
+import dj_database_url
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
-
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'default': dj_database_url.config(
+        default='sqlite:///db.sqlite3',
+        conn_max_age=600
+    )
 }
 
 # Static files (CSS, JavaScript, Images)
